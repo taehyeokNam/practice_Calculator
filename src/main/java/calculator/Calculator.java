163b142499b9659f15fe5921a33da07f183f48d5
navyth91 <nayth91@naver.com>
2024-07-29 17:08:28 +0900

    2-1_Calculator_클래스_생성

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,71 @@
 package calculator;
 
+import java.util.*;
+
 public class Calculator {
 
+    private Queue<Integer> Result = new LinkedList<Integer>();
+
+    //두 수의 덧셈
+    public int Plus (int input1, int input2) {
+
+        this.Result.add(input1 + input2);
+        System.out.println(input1 + " + " + input2 + " = " + (input1 + input2));
+
+        return (input1 + input2);
+    }
+
+    //두 수의 뺄셈
+    public int Sub (int input1, int input2) {
+
+        this.Result.add(input1 - input2);
+        System.out.println(input1 + " - " + input2 + " = " + (input1 - input2));
+
+        return (input1 - input2);
+    }
+
+    //두 수의 곱셈
+    public int Multi (int input1, int input2) {
+
+        this.Result.add(input1 * input2);
+        System.out.println(input1 + " * " + input2 + " = " + (input1 * input2));
+
+        return (input1 * input2);
+    }
+
+    //input1을 input2로 나눈 몫
+    public int Div (int input1, int input2) {
+
+        this.Result.add(input1 / input2);
+        System.out.println(input1 + " / " + input2 + " = " + (input1 / input2));
+
+        return (input1 / input2);
+    }
+
+    //계산 결과 지우기
+    void Delete() {
+        this.Result.remove();
+    }
+
+    //계산 결과 모두 보여주기
+    void DisplayResult() {
+        for (int result : this.Result)
+            System.out.println(result);
+    }
+
+    public Calculator(int input1, int input2, char sign) throws CalculatorException{
+
+        if (sign == '/' && input2 == 0) {
+            //분모가 0일 때
+            throw new CalculatorException();
+        }
+        else if (sign != '+' && sign != '-' &&
+                sign != '*' && sign != '/') {
+            //사칙연산 이외의 기호가 들어왔을 때
+            throw new CalculatorException();
+        }
+    };
+
 }
+
+
---
+++ src/main/java/calculator/CalculatorException.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class CalculatorException extends Exception{
+    public CalculatorException(){
+        System.out.println("잘못된 입력입니다.");
+    }
+}
